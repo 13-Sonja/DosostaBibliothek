8c87c5206dcf87c6b3bec4a9afc9215a95667e1c
Sonja Ruth <donnie.darko1966@web.de>
2025-01-24 13:23:51 +0100

    Projektbasis

---
+++ DosostaApp/0TemplateForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace DosostaApp
+{
+    partial class _0TemplateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Cascadia Mono", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(199, 70);
+            label1.Name = "label1";
+            label1.Size = new Size(434, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Basisform für alle neuen Forms";
+            // 
+            // _0TemplateForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 244, 233);
+            ClientSize = new Size(914, 510);
+            Controls.Add(label1);
+            Font = new Font("Cascadia Mono", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Name = "_0TemplateForm";
+            Text = "Dososta Bibliothek";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
